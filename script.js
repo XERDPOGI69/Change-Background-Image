6701a1dd93ae020eadb4bd29fd24c132f970dd77
XERDPOGI69 <joshportacion9@gmail.com>
2023-11-05 02:06:11 +0800

    5th

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-const fileInput = document.getElementById("fileInput");
-const changeBackgroundButton = document.getElementById("changeBackgroundButton");
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+    apiKey: "AIzaSyDhgsiTVcqTdNjawLIsnNw2k21tpwWd0PI",
+    authDomain: "aqueous-impact-351219.firebaseapp.com",
+    databaseURL: "https://aqueous-impact-351219-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "aqueous-impact-351219",
+    storageBucket: "aqueous-impact-351219.appspot.com",
+    messagingSenderId: "678046794632",
+    appId: "1:678046794632:web:b924a8143bb113ba40703d",
+    measurementId: "G-4RYXJ6W9WM"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
 
+  function saveUserPreferences(userId, backgroundImageUrl) {
+    db.collection("users").doc(userId).set({ backgroundImageUrl });
+  }
+  
 
-const savedBackgroundImage = localStorage.getItem("backgroundImage");
-if (savedBackgroundImage) {
-    document.body.style.backgroundImage = savedBackgroundImage;
-}
-
-changeBackgroundButton.addEventListener("click", function () {
-    fileInput.click();
-});
-
-fileInput.addEventListener("change", function () {
-    const file = fileInput.files[0];
-    if (file) {
-        const imageUrl = URL.createObjectURL(file);
-        document.body.style.backgroundImage = `url(${imageUrl})`;
-
-       
-        localStorage.setItem("backgroundImage", `url(${imageUrl})`);
-    }
-});
+  function getUserPreferences(userId) {
+    return db.collection("users").doc(userId).get().then((doc) => {
+      if (doc.exists) {
+        return doc.data().backgroundImageUrl;
+      }
+      return null;
+    });
+  }
+  
